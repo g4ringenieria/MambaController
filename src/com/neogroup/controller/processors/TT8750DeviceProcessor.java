4baed068c06b1d40f4ae15b2b252600d772152c3
Luis <luis.amengual@sitrack.com>
2014-07-14 15:15:03 -0300

    Cambios para ingresar reportes desde el procesador tt8750

--- src/com/neogroup/controller/processors/TT8750DeviceProcessor.java
+++ src/com/neogroup/controller/processors/TT8750DeviceProcessor.java
@@ -74,40 +74,32 @@ public class TT8750DeviceProcessor extends Processor implements ConnectionListen
                     int odometer = (ord(data[44]) + (ord(data[43]) << 8) + (ord(data[42]) << 16) + (ord(data[41]) << 24)) / 10;
                     Calendar date = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
                     date.set(ord(data[45])+2000, ord(data[46]) - 1, ord(data[47]), ord(data[48]), ord(data[49]), ord(data[50]));
+                    
+                    StringBuilder reportJson = new StringBuilder();
+                    reportJson.append('{');
+                    reportJson.append("\"reportClassType\": ").append(1);
+                    reportJson.append(",\"reportTypeId\": ").append(reportTypeId);
+                    reportJson.append(",\"deviceId\": ").append(deviceId);
+                    reportJson.append(",\"validity\": ").append(validity);
+                    reportJson.append(",\"latitude\": ").append(latitude);
+                    reportJson.append(",\"longitude\": ").append(longitude);
+                    reportJson.append(",\"speed\": ").append(speed);
+                    reportJson.append(",\"course\": ").append(course);
+                    reportJson.append(",\"altitude\": ").append(altitude);
+                    reportJson.append(",\"odometer\": ").append(odometer);
+                    reportJson.append(",\"date\": ").append(date.getTimeInMillis());
+                    reportJson.append('}');
+//                    Runtime.getRuntime().exec("php ../../NeoGroup/command.php " + reportJson.toString());
                 }
                 break;
-//            case self::DATAGRAMTYPE_SERIALPORT:
+            case DATAGRAMTYPE_SERIALPORT:
 //                $datagram = substr($datagram, 7);
-//		$datagram = "0035000a08100418e0be4f" . $datagram;
+//		  $datagram = "0035000a08100418e0be4f" . $datagram;
 //                $datagram = hex2bin($datagram);
 //                $deviceId = intval((substr($datagram, 15, 8)));
 //                $eventId = ord($datagram{14}) + (ord($datagram{13}) << 8);
 //                $this->getLogger()->debug("Serial port package => equipo: " . $deviceId . "; evento: " . $eventId);
-//                
-//                $ios = ord($datagram{24}) + (ord($datagram{23}) << 8);
-//                $validity = ord($datagram{25});
-//                $latitude = $this->getCoordinate(substr($datagram, 26, 4));
-//                $longitude = $this->getCoordinate(substr($datagram, 30, 4));
-//                $speed = (intval(ord($datagram{35}) + (ord($datagram{34}) << 8))/10) * 1.8;
-//                $course = (intval(ord($datagram{37}) + (ord($datagram{36}) << 8))/10);
-//                $altitude = (ord($datagram{40}) + (ord($datagram{39}) << 8) + (ord($datagram{38}) << 16)) / 10;
-//                $odometer = ord($datagram{44}) + (ord($datagram{43}) << 8) + (ord($datagram{42}) << 16) + (ord($datagram{41}) << 24);
-//                $date = new DateTime();
-//                $date->setDate(ord($datagram{45})+2000, ord($datagram{46}), ord($datagram{47}));
-//                $date->setTime(ord($datagram{48}), ord($datagram{49}), ord($datagram{50}));
-//                $report = new PositionReport();
-//                $report->setDevice(new Device($deviceId));
-//                $report->setReportType(new ReportType($this->getReportTypeByEvent($eventId)));
-//                $report->setLongitude($longitude);
-//                $report->setLatitude($latitude);
-//                $report->setAltitude($altitude);
-//                $report->setSpeed($speed);
-//                $report->setCourse($course);
-//                $report->setDate($date);
-//                $report->setInputDate(new DateTime());
-//                $report->setOdometer($odometer);
-//                $report->insert();
-//                break;
+                break;
         }
     }
 
