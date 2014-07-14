b2b10b2b86f6e19e8356b9878ec936a4f27c31ab
Luis <luis.amengual@sitrack.com>
2014-07-14 18:51:53 -0300

    Cambios menores

--- src/com/neogroup/controller/processors/TT8750DeviceProcessor.java
+++ src/com/neogroup/controller/processors/TT8750DeviceProcessor.java
@@ -91,7 +91,8 @@ public class TT8750DeviceProcessor extends Processor implements ConnectionListen
                     reportJson.append(",\"odometer\": ").append(odometer);
                     reportJson.append(",\"date\": ").append(date.getTimeInMillis());
                     reportJson.append('}');
-                    Runtime.getRuntime().exec("php ../../NeoGroup/command.php reports/createResource '" + reportJson.toString() + "'");
+                    
+                    Runtime.getRuntime().exec(new String[] {"php", "../../NeoGroup/command.php", "reports/createResource", reportJson.toString()});
                 }
                 break;
             case DATAGRAMTYPE_SERIALPORT:
