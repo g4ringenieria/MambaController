8b17f537af7ee44ca2604aedefbf4b23d9a00cae
Luis <luis.amengual@sitrack.com>
2014-07-16 15:39:01 -0300

    Simplificacion de procesadores

--- src/com/neogroup/controller/Application.java
+++ src/com/neogroup/controller/Application.java
@@ -4,11 +4,16 @@ package com.neogroup.controller;
 import com.neogroup.controller.processors.ConnectionsProcessor;
 import com.neogroup.controller.processors.GeneralProcessor;
 import com.neogroup.controller.processors.Processor;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 
 public class Application 
 {
@@ -133,7 +138,29 @@ public class Application
             try
             {   
                 FileHandler handler = new FileHandler("log_" + getName() + ".txt", 1024000, 1, true);
-                handler.setFormatter(new SimpleFormatter());
+                handler.setFormatter(new Formatter() 
+                {       
+                    private final Date date = new Date();
+                    private final SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    
+                    @Override
+                    public String format(LogRecord record) 
+                    {
+                        date.setTime(record.getMillis());
+                        String message = formatMessage(record);
+                        String throwable = "";
+                        if (record.getThrown() != null) 
+                        {
+                            StringWriter sw = new StringWriter();
+                            PrintWriter pw = new PrintWriter(sw);
+                            pw.println();
+                            record.getThrown().printStackTrace(pw);
+                            pw.close();
+                            throwable = sw.toString();
+                        }
+                        return String.format("[%s] %s: %s %s\n", dateFormatter.format(date), record.getLevel().getLocalizedName(), message, throwable);
+                    }
+                });
                 logger.setUseParentHandlers(false);
                 logger.addHandler(handler);
             } 
